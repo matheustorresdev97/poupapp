1f10a00c2b453ec560e72fc4c59e65f4afff004d
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-20 21:20:03 -0300

    feat: botoes de radio dinamizados com keyValuePipe

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,31 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from '../../../compartilhados/modal/modal.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
+import { KeyValuePipe } from '@angular/common';
+import { TipoTransacao } from '../../compartilhados/transacao.model';
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent, FormsModule],
+  imports: [BotaoComponent, ModalComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css',
 })
 export class BotaoAdicionarTransacaoComponent {
   modalAberto = signal(false);
 
-   novaTransacaoForm = {
+  novaTransacaoForm = {
     nome: '',
     tipo: '',
     valor: '',
     data: '',
-    conta: ''
-  }
+    conta: '',
+  };
 
+  tiposTransacao = TipoTransacao;
 
   abrirModal() {
     this.modalAberto.set(true);
   }
 
-    aoSubmeter() {
+  aoSubmeter() {
     console.log(this.novaTransacaoForm);
   }
 }
