cf3b7b6544523405b641dabf8f1b8e1fad5fd5dd
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-20 21:18:15 -0300

    feat: dinamizando o form de nova transação

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,17 +1,31 @@
-import { Component, signal, effect } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from '../../../compartilhados/modal/modal.component';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css',
 })
 export class BotaoAdicionarTransacaoComponent {
   modalAberto = signal(false);
 
+   novaTransacaoForm = {
+    nome: '',
+    tipo: '',
+    valor: '',
+    data: '',
+    conta: ''
+  }
+
+
   abrirModal() {
     this.modalAberto.set(true);
+  }
+
+    aoSubmeter() {
+    console.log(this.novaTransacaoForm);
   }
 }
